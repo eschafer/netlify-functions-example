5f68be0250792d3d17d8357e37f71ad1a85d07f0
Elizabeth Schafer <elizabeth.schafer.wenk@gmail.com>
2019-08-22 08:47:33 -0400

    Make sure js doesn't run twice

--- webpack.client.js
+++ webpack.client.js
@@ -11,7 +11,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
+      template: "./src/index.html",
+      inject: false
     })
   ],
   devServer: {
